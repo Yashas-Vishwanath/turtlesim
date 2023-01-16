23ec1c5a16ae3c967509c54e0b9bdee72708e7bb
Yashas-Vishwanath <122467316+Yashas-Vishwanath@users.noreply.github.com>
2023-01-16 13:41:47 +0000

    talker&listener_working

--- beginner_tutorials/scripts/talker.py
+++ beginner_tutorials/scripts/talker.py
@@ -1,17 +1,17 @@
-#!/usn/bin/env python3
-
+#!/usr/bin/env python3
+# license removed for brevity
 import rospy
 from std_msgs.msg import String
 
 
 def talker():
-    rospy.init_node('talker', anonymous=True)
     pub = rospy.Publisher('chatter', String, queue_size=10)
-    rate = rospy.Rate(10)
+    rospy.init_node('talker', anonymous=True)
+    rate = rospy.Rate(10)  # 10hz
     while not rospy.is_shutdown():
         hello_str = "hello world %s" % rospy.get_time()
         rospy.loginfo(hello_str)
-        pub.publisher(hello_str)
+        pub.publish(hello_str)
         rate.sleep()
 
 
